9c7793cd5a3568802c92ac91e22d89111e29c2ac
nehagarde <neha.r.garde@gmail.com>
2019-06-26 15:29:59 +0530

    Committing Project Setup

--- BookWeb/Book/models.py
+++ BookWeb/Book/models.py
@@ -12,3 +12,20 @@ class notify_me(models.Model):
         cursor.execute("SELECT * FROM book_notify_me where user_mail = '" + user_email + "' AND book_title = '" + book_title + "' ")
         return cursor.fetchall()
 
+    def update_ac_price(self, user_email, book_title, ac_price):
+        with connection.cursor() as cursor:
+            cursor.execute("UPDATE book_notify_me SET alert_price = " + ac_price +" WHERE user_mail=" + user_email + " and book_title='" + book_title + "'" )
+
+        return 1
+
+    def user_data_all(self):
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM book_notify_me")
+
+        return cursor.fetchall()
+
+    def updated_is_notified(self, user_email, book_title):
+        with connection.cursor as cursor:
+            cursor.execute("UPDATE book_notify_me SET is_notified = 'F' WHERE user_mail = '" + user_email + "' AND book_title='" + book_title + "' ")
+
+        return 1
